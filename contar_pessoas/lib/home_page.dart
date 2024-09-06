4893cb94fd963fb08549eb468bbca4c5ff245cd3
wesley gomes <wesleypgomes2020@gmail.com>
2024-09-06 15:26:47 -0300

    começo lgc programaçao

--- contar_pessoas/lib/home_page.dart
+++ contar_pessoas/lib/home_page.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int contador = 0;
+
   void decremento() {
+    contador--;
     print("decremento");
   }
 
   void incremento() {
+    contador++;
     print("incremento");
   }
 
@@ -14,48 +24,69 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red.shade300,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Pode entrar!",
-            style: TextStyle(
-              fontSize: 42,
-              fontWeight: FontWeight.bold,
-              color: Colors.white
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('assets/images/fundo.jpg'),
+          fit: BoxFit.cover,
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Pode entrar!",
+              style: TextStyle(
+                  fontSize: 42,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+             Padding(
+              padding: const EdgeInsets.all(50),
+              child: Text(
+                "$contador",
+                style: const TextStyle(fontSize: 100, color: Colors.white),
               ),
-          ),
-          const Text(
-            "0",
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white
-              ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decremento,
-                child: const Text("Sair",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size(100, 100),
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24))),
+                  onPressed: decremento,
+                  child: const Text(
+                    "Sair",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
                     ),
                   ),
-              ),
-              TextButton(
-                onPressed: incremento,
-                child: const Text("Entrar",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
+                ),
+                const SizedBox(width: 32),
+                TextButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: const Size(100, 100),
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(24))),
+                  onPressed: incremento,
+                  child: const Text(
+                    "Entrar",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
                     ),
                   ),
-              )
-            ],
-          ),
-        ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
