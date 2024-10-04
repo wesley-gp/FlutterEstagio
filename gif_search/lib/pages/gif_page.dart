3ffbb0e6ae51548a483ad03a8bdc6060dec5c41f
wesley gomes <wesleypgomes2020@gmail.com>
2024-10-04 14:17:37 -0300

    share add

--- gif_search/lib/pages/gif_page.dart
+++ gif_search/lib/pages/gif_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 class GifPage extends StatelessWidget {
   const GifPage({super.key, required this.gifMap});
@@ -8,9 +9,16 @@ class GifPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                Share.share(gifMap['images']['fixed_height']['url']);
+              },
+              icon: const Icon(Icons.share))
+        ],
         foregroundColor: Colors.white,
         titleTextStyle: const TextStyle(
-           fontSize: 18,
+          fontSize: 18,
         ),
         title: Text(gifMap['title']),
         backgroundColor: Colors.black,
